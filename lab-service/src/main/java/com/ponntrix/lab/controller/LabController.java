5560e2cb0a553ceb90cf29df06041a6b8ec89639
Tejaswini <penjurutejaswini3@gmail.com>
2026-08-10 16:58:04 +0530

    modified service port numbers

--- lab-service/src/main/java/com/ponntrix/lab/controller/LabController.java
+++ lab-service/src/main/java/com/ponntrix/lab/controller/LabController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LabController {
 
     @GetMapping
-    private String test(){
+    public String test(){
         return  "lab controller";
     }
 }
